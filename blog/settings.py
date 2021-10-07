20da6cb1a6d8fc3c4cf5fcc2e4abf53c00a5d108
Vinicius de Lima Gomes <vinicius.gomes@sulamerica.com.br>
2021-10-07 18:41:05 -0300

    insercao django_summernote

--- blog/settings.py
+++ blog/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-summernote',
+    'django_summernote',
 ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
