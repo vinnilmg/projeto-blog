2176125d217d799f0519247534c23f5570e2a3ec
Vinicius de Lima Gomes <vinicius.gomes@sulamerica.com.br>
2021-10-17 21:57:36 -0300

    recaptcha

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,14 +1,33 @@
 from django.forms import ModelForm
 from .models import Comentario
+import requests
 
 
 class FormComentario(ModelForm):
     def clean(self):
-        data = self.cleaned_data
+        raw_data = self.data
+        recaptcha = raw_data.get('g-recaptcha-response')
 
-        nome = data.get('nome_comentario')
-        email = data.get('email_comentario')
-        comentario = data.get('comentario')
+        # verificação do captcha
+        captcha_req_json = {
+                'secret': '6Len_dgcAAAAAOuXg9gxChlQB-ZVeYdVcuBdb8_y',
+                'response': recaptcha
+            }
+        recaptcha_response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=captcha_req_json).json()
+        # print(recaptcha_response)
+
+        if not recaptcha_response.get('success'):
+            self.add_error(
+                'comentario',
+                'Desculpe Mr. Robot, ocorreu um erro no captcha.'
+            )
+
+        # pegando valors do form
+        cleaned_data = self.cleaned_data
+
+        nome = cleaned_data.get('nome_comentario')
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
 
         if len(nome) < 5:
             self.add_error(
